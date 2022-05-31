f816a292b33cf2819a94c78cb9fb422cde546e58
Pedro Faustino <faustinopedro.faustino@gmail.com>
2022-05-30 21:32:24 -0300

    added new to init.vim

--- config-neovim.sh
+++ config-neovim.sh
@@ -7,20 +7,5 @@ fi
 #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sudo curl "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -o autoload/plug.vim
-
-echo "
-set number
-set nocompatible
-call plug#begin('~/.config/nvim/plugged')
-Plug 'morhetz/gruvbox'
-Plug 'preservim/nerdtree'
-Plug 'ctrlpvim/ctrlp.vim'
-Plug 'neoclide/coc.nvim', {'branch': 'release'}
-Plug 'tpope/vim-fugitive'
-Plug 'christoomey/vim-tmux-navigator'
-Plug 'ryanoasis/vim-devicons'
-call plug#end()
-colorscheme gruvbox
-map <silent> <C-n> :NERDTreeFocus<CR>
-" >> init.vim
+vim +'PlugInstall --sync' +qa
 #rm config-neovim.sh
--- init.vim
+++ init.vim
@@ -9,7 +9,20 @@ Plug 'neoclide/coc.nvim', {'branch': 'release'}
 Plug 'tpope/vim-fugitive'
 Plug 'christoomey/vim-tmux-navigator'
 Plug 'ryanoasis/vim-devicons'
+Plug 'scrooloose/nerdcommenter'
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate' }
 call plug#end()
 colorscheme gruvbox
 map <silent> <C-n> :NERDTreeFocus<CR>	
-
+set nowrap 
+set smartcase 
+set hlsearch
+set noerrorbells
+set tabstop=2 softtabstop=2
+set expandtab
+set smartindent
+syntax on
+command! -nargs=0 Prettier :CocCommand prettier.formatFile
+vmap ++ <plug>NERDCommenterToggle
+nmap ++ <plug>NERDCommenterToggle
+let g:NERDTreeIgnoe = ['^node_modules$']
