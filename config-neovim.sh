33140fd79373f04b720f7508fd88d0a5ad196033
Pedro Faustino <faustinopedro.faustino@gmail.com>
2022-05-30 21:41:25 -0300

    Adiciona resposta para usuário

--- config-neovim.sh
+++ config-neovim.sh
@@ -2,10 +2,12 @@
 
 if [ ! -d autoload ]
 then
+        echo "Criando diretório autoload para configurar o vim-plug..."
          mkdir autoload
 fi
 #sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
 #        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "Instalando o vim-plug..."
 sudo curl "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" -o autoload/plug.vim
-vim +'PlugInstall --sync' +qa
+nvim +'PlugInstall --sync' +qa
 #rm config-neovim.sh
